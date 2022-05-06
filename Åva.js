314ca36c656ba9bf3373d0bfe091b1dbfd45a549
Theodor Henriksson <theodor.henriksson@skola.taby.se>
2022-05-06 14:55:09 +0200

    i don't know

--- Åva.js
+++ Åva.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function burgerFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -13,3 +13,37 @@ const wrapper = document.getElementById('burger_icon')
 wrapper.addEventListener("click", () => {
   wrapper.classList.toggle("open")
 })
+
+
+
+
+
+
+
+
+
+
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
+
+function iconFunction(x) {
+  x.classList.toggle("fas fa-xmark");
+}
+
+
+
+
