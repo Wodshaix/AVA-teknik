6035a6de2d211ed9744445a1a14ae4e2d7c808c1
Theodor Henriksson <theodor.henriksson@skola.taby.se>
2022-05-21 00:02:29 +0200

    done

---
+++ kurschema.js
@@ -0,0 +1,22 @@
+function burgermenu(x) {
+  x.classList.toggle("change");
+}
+
+function burgerLinks() {
+  var x = document.getElementById("burgerLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+  
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
